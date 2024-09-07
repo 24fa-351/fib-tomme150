a16bc92d1d9caf40e83dbc19d38971a49a9c53c5
tomme150 <tle150@csu.fullerton.edu>
2024-09-06 23:46:32 -0700

    Set up the three arguments

--- fib.c
+++ fib.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-   printf("Hello!\n");
+int main(int argc, char *argv[]) {
+   if (argc != 4) {
+      printf("Please type in an integer, method(i or r), and filename.txt\n", argv[0]);
+      return 1;
+   }
+   
+   int number = atoi(argv[1]);
+   char *method = argv[2];
+   char *fileName = argv[3];
+
    return 0;
 }
