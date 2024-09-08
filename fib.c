8aa5e2fef4ec0ce00d5c0b9d4d6f7c78ee22ee4e
tomme150 <tle150@csu.fullerton.edu>
2024-09-07 23:16:56 -0700

    Working, passed 3 of 4 test

--- fib.c
+++ fib.c
@@ -11,6 +11,20 @@ int recursive(int n) {
    }
 }
 
+int iterative(int n) {
+    if (n <= 1) {
+      return n;
+    }
+   
+    int a = 0, b = 1, fib = 0;
+    for (int i = 2; i <= n; i++) {
+        fib = a + b;
+        a = b;
+        b = fib;
+      }
+    return fib;
+}
+
 int main(int argc, char *argv[]) {
    if (argc != 4) {
       printf("Please type in an integer, method(i or r), and filename.txt\n", argv[0]);
@@ -39,7 +53,7 @@ int main(int argc, char *argv[]) {
    // printf("%s\n", fileName);
    // printf("%d\n", file_int);
 
-   int N = number + file_int;
+   int N = number + file_int - 1;
 
    // Debugging
    // printf("%d\n", N);
@@ -49,7 +63,7 @@ int main(int argc, char *argv[]) {
    if (strcmp(method, "r")  == 0) {
       results = recursive(N);
    } else if (strcmp(method, "r")  == 0) {
-      
+      results = iterative(N);
    } else {
       printf("Use 'r' for recursive or 'i' for iterative.\n");
       return 1;
